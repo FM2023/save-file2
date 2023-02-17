80692593b52aa32f93defa7998029972db1450ad
SEONG-HYUN LEE <122852215+FM2023@users.noreply.github.com>
2023-02-18 00:23:43 +0900

    Add files via upload

--- ContentView.swift
+++ ContentView.swift
@@ -1,41 +1,94 @@
-//
-//  ContentView.swift
-//  HelloXcodeSwift
-//
-//  Created by 이은권 on 2023/01/01.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    @State var data: String = "0"
-    
     var body: some View {
         VStack {
-            HStack {
-                Text("이성현")
-                    .font(.largeTitle)
-                    .bold()
-                Image(systemName: "heart")
-                    .foregroundColor(.red)
-                    .bold()
-                Text("전채리")
-                    .font(.largeTitle)
-                    .bold()
-            }
-            Text(data)
-                .bold()
-                .foregroundColor(.green)
-                Button {
-                    data = "\(Int.random(in:1...45))"
-                } label: {
-                    Text("update")
+            HeaderView()
+            HStack (spacing: 15){
+                PricingView(title: "Basic", price: "$9", textColor: .white, bgColor: .purple)
+                
+                ZStack {
+                    PricingView(title: "Pro", price: "$19", textColor: .black, bgColor: Color(red: 240/255, green: 240/255, blue: 240/255))
+                    Text("Best for designer")
+                        .font(.system(.caption, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(5)
+                        .background(Color(red:255/255, green: 183/255, blue: 37/255))
+                        .offset(x:0, y: 87)
                 }
             }
+            .padding(.horizontal)
+            ZStack {
+                PricingView(title: "Team", price: "$299", textColor: .white, bgColor: Color(red: 62/255, green: 63/255, blue: 70/255), icon: "wand.and.rays")
+                    .padding()
+                Text("Perfect for teams with 20 members")
+                    .font(.system(.caption, design: .rounded))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(5)
+                    .background(Color(red:255/255, green: 183/255, blue: 37/255))
+                    .offset(x:0, y: 110)
+            }
+            Spacer()
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct HeaderView: View {
+    var body: some View {
+        HStack {
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("Choose")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .fontWeight(.black)
+                    Text("Your Plan")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .fontWeight(.black)
+                }
+            Spacer()
+            }
+        .padding()
         }
     }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View { ContentView()
+struct PricingView: View {
+    
+    var title: String
+    var price: String
+    var textColor: Color
+    var bgColor: Color
+    var icon: String?
+    
+    var body: some View {
+        VStack{
+            
+            if let icon = icon {
+                Image(systemName: icon)
+                    .font(.largeTitle)
+                    .foregroundColor(textColor)
+            }
+            Text(title)
+                .font(.system(.largeTitle, design: .rounded))
+                .fontWeight(.black)
+                .foregroundColor(textColor)
+            Text(price)
+                .font(.system(size: 40, weight: .heavy, design: .rounded))
+                .foregroundColor(textColor)
+            Text("per month")
+                .font(.headline)
+                .foregroundColor(textColor)
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+        .padding(40)
+        .background(bgColor)
+        .cornerRadius(10)
     }
 }
